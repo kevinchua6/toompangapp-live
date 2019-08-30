77abdde3deccf1dec71307f3f1ebf67f37a433bd
shin <shin@shins-MacBook-Pro.local>
2019-08-30 23:19:32 +0800

    Added basic index.js and index.html templates. Added google map functionalites. Api key not working

---
+++ index.js
@@ -0,0 +1,7 @@
+//jshint esversion:6
+
+const express = require("express");
+const app = express();
+var googleMapsClient = require('@google/maps').createClient({
+  key: 'AIzaSyAvTWlN0IC6Ck-xqfLvRG5TvTPnAHB8cWs'
+});
