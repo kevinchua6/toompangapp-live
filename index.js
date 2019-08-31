5470a3d63571f045b3af45e3ff0f605e216988ec
skullspine9@gmail.com <skullspine9@gmail.com>
2019-08-31 15:39:18 +0800

    added search bar

--- index.js
+++ index.js
@@ -6,33 +6,31 @@ var googleMapsClient = require('@google/maps').createClient({
 });
 
 
-// Initialize and add the map
-function initMap() {
-  // The location of Uluru
-  var Singapore = {
-    lat: 	1.43281,
-    lng: 103.77959
-  };
-  // The map, centered at Uluru
-  var map = new google.maps.Map(
-    document.getElementById('map'), {
-      zoom: 4,
-      center: Singapore
-    });
-  // The marker, positioned at Uluru
-  var marker = new google.maps.Marker({
-    position: Singapore,
-    map: map
-  });
 
-
-}
 
   function initAutocomplete() {
+    //function initMap Initialize and add the map
+    // The location of Uluru
+    var Singapore = {
+      lat: 	1.43281,
+      lng: 103.77959
+    };
+    // The map, centered at Uluru
+    var map = new google.maps.Map(
+      document.getElementById('map'), {
+        zoom: 4,
+        center: Singapore
+      });
+    // The marker, positioned at Uluru
+    var marker = new google.maps.Marker({
+      position: Singapore,
+      map: map
+    });
+
 // Create the search box and link it to the UI element.
   var input = document.getElementById('pac-input');
-  var searchBox = new google.amps.places.SearchBox(input);
-  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+  var searchBox = new google.maps.places.SearchBox(input);
+  //map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
 // Bias the SearchBox results towards current map's viewport.
   map.addListener('bounds_changed', function() {
