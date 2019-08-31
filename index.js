b2e2ae274f1ceee7b7ece82b14352ed933652cb8
shin <shin@shins-MacBook-Pro.local>
2019-08-31 14:47:19 +0800

    Changed height for maps.

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 //jshint esversion:6
 
-const express = require("express");
-const app = express();
 var googleMapsClient = require('@google/maps').createClient({
   key: 'AIzaSyAvTWlN0IC6Ck-xqfLvRG5TvTPnAHB8cWs'
 });
@@ -25,4 +23,6 @@ function initMap() {
     position: Singapore,
     map: map
   });
+
+
 }
