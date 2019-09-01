e5aff07a772cb047c4c79d072158b84b1fee1dea
yourally2 <yourally2@users.noreply.github.com>
2019-09-01 19:37:17 +0800

    simplied code

--- README.md
+++ README.md
@@ -41,6 +41,34 @@ Used for people to rent and list cars.
 # Commented out codes
 <br>
 
+###Index.js  
+Code to place icons and markers when search  
+```
+var icon = {
+ url: place.icon,
+ size: new google.maps.Size(71, 71),
+ origin: new google.maps.Point(0, 0),
+ anchor: new google.maps.Point(17, 34),
+ scaledSize: new google.maps.Size(25, 25)
+};
+
+// Create a marker for each place.
+markers.push(new google.maps.Marker({
+ map: map,
+ icon: icon,
+ title: place.name,
+ position: place.geometry.location
+}));
+```
+Code to clear old markers upon search  
+```
+// Clear out the old markers.
+ markers.forEach(function(marker) {
+   marker.setMap(null);
+ });
+ markers = [];
+ ```
+
 ### Code that doesn't work (start of index.js)
 ```
 var googleMapsClient = require('@google/maps').createClient({
--- index.js
+++ index.js
@@ -35,7 +35,7 @@ function initAutocomplete() {
   function handleLocationError(browserHasGeolocation, infoWindow, pos) {
     infoWindow.setPosition(pos);
     infoWindow.setContent(browserHasGeolocation ?
-      'Error: The Geolocation service failed.' :
+      'Error: Cannot find your location.' :
       'Error: Your browser doesn\'t support geolocation.');
     infoWindow.open(map);
   }
@@ -44,7 +44,6 @@ function initAutocomplete() {
   // Create the search box and link it to the UI element.
   var input = document.getElementById('pac-input');
   var searchBox = new google.maps.places.SearchBox(input);
-  //map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
   // Bias the SearchBox results towards current map's viewport.
   map.addListener('bounds_changed', function() {
@@ -60,12 +59,6 @@ function initAutocomplete() {
       return;
     }
 
-    // Clear out the old markers.
-    markers.forEach(function(marker) {
-      marker.setMap(null);
-    });
-    markers = [];
-
     // For each place, get the icon, name and location.
     var bounds = new google.maps.LatLngBounds();
     places.forEach(function(place) {
@@ -73,21 +66,7 @@ function initAutocomplete() {
         console.log("Returned place contains no geometry");
         return;
       }
-      var icon = {
-        url: place.icon,
-        size: new google.maps.Size(71, 71),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(17, 34),
-        scaledSize: new google.maps.Size(25, 25)
-      };
 
-      // Create a marker for each place.
-      markers.push(new google.maps.Marker({
-        map: map,
-        icon: icon,
-        title: place.name,
-        position: place.geometry.location
-      }));
 
       if (place.geometry.viewport) {
         // Only geocodes have viewport.
