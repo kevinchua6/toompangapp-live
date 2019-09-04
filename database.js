18f2c1c5bf0488870d29f7f936a01bce60541e61
yourally2 <yourally2@users.noreply.github.com>
2019-09-04 22:56:18 +0800

    changed minor things

--- database.js
+++ database.js
@@ -1,14 +1,19 @@
 // Initialize the Realtime Database JavaScript SDK : https://firebase.google.com/docs/database/web/start#initialize_the_javascript_sdk
 
-// Set the configuration for your app
-  // TODO: Replace with your project's config object
-  var config = {
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#config-web-app
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
     apiKey: "AIzaSyAMxBw81NHhTKNyws2Y4zGI25G-RAiUyB8",
     authDomain: "toompang-web.firebaseapp.com",
-    databaseURL: "https://toompang-web.firebaseio.com/",
-    storageBucket: "bucket.appspot.com"
+    databaseURL: "https://toompang-web.firebaseio.com",
+    projectId: "toompang-web",
+    storageBucket: "",
+    messagingSenderId: "1043268270194",
+    appId: "1:1043268270194:web:79a982514c45923d"
   };
-  firebase.initializeApp(config);
-
-  // Get a reference to the database service
-  var database = firebase.database();
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
--- index.js
+++ index.js
@@ -1,46 +1,48 @@
 //jshint esversion:6
+var map, infoWindow;
 
-function initAutocomplete() {
-  var map, infoWindow;
-    map = new google.maps.Map(document.getElementById('map'), {
-      center: {
-        lat: 1.306,
-        lng: 103.829
-      },
-      zoom: 17
+function initMap(){
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {
+      lat: 1.306,
+      lng: 103.829
+    },
+    zoom: 17
+  });
+  infoWindow = new google.maps.InfoWindow;
+
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Location found.');
+      infoWindow.open(map);
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
     });
-    infoWindow = new google.maps.InfoWindow;
-
-    // Try HTML5 geolocation.
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function(position) {
-        var pos = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude
-        };
-
-        infoWindow.setPosition(pos);
-        infoWindow.setContent('Location found.');
-        infoWindow.open(map);
-        map.setCenter(pos);
-      }, function() {
-        handleLocationError(true, infoWindow, map.getCenter());
-      });
-    } else {
-      // Browser doesn't support Geolocation
-      handleLocationError(false, infoWindow, map.getCenter());
-    }
-
-
-  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-    infoWindow.setPosition(pos);
-    infoWindow.setContent(browserHasGeolocation ?
-      'Error: Cannot find your location.' :
-      'Error: Your browser doesn\'t support geolocation.');
-    infoWindow.open(map);
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
   }
 
+  initAutocomplete();
+}
 
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+    'Error: Cannot find your location.' :
+    'Error: Your browser doesn\'t support geolocation.');
+  infoWindow.open(map);
+}
+
+function initAutocomplete() {
   // Create the search box and link it to the UI element.
   var input = document.getElementById('pac-input');
   var searchBox = new google.maps.places.SearchBox(input);
